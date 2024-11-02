4deb06d08e80422d1580270c19d8f70d0115af4d
firdous alam <firdous.alam2058@gmail.com>
2024-11-02 21:03:37 +0530

    creating typescript based project

---
+++ typescript/first-app.ts
@@ -0,0 +1,18 @@
+/*
+Not allowed in ts
+let userName = "Firdous";
+userName = 10;
+*/
+
+/*
+allowed in ts
+let userName;
+userName = "Firdous";
+userName = 10;
+*/
+/*
+way to declare variable in ts
+let userName:string; //explicite type assignment
+userName = "Firdous";
+
+*/
