79f42db59b2e92bfa88172a70d1a9732d7ada585
firdous alam <firdous.alam2058@gmail.com>
2024-11-03 14:03:29 +0530

    react Pops Using Children

--- reactTypescript/src/App.tsx
+++ reactTypescript/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import CourseGoal from './components/CourseGoal';
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//   return (
+//     <main>
+//       <CourseGoal
+//        title="learn React +TS"
+//        description ="Learn it from Technophile Firdous" >
+
+//        </CourseGoal>
+//     </main>
+//   )
+// }
+
+// if we want to use children to send data to components
+return (
+  <main>
+    <CourseGoal
+     title="learn React +TS"
+    >
+      <p>Learn it from Technophile Firdous</p>
+      
+     </CourseGoal>
+  </main>
+)
 }
 
 export default App
---
+++ reactTypescript/src/components/CourseGoal.tsx
@@ -0,0 +1,73 @@
+// using object way
+// export default function CourseGoal(props : {
+//     title : string,
+//     description : string
+// }){
+//     return <article>
+//         <div>
+//             <h2>
+//                 {props.title}
+//             </h2>
+//             <p>{props.description}</p>
+//         </div>
+//         <button>DELETE</button>
+//     </article>
+// }
+
+import { type ReactNode } from "react";
+
+//using destructuring
+// export default function CourseGoal({title,description} : {
+//     title : string,
+//     description : string
+// }){
+//     return <article>
+//         <div>
+//             <h2>
+//                 {title}
+//             </h2>
+//             <p>{description}</p>
+//         </div>
+//         <button>DELETE</button>
+//     </article>
+// }
+
+//declaring type defination using types or interface
+// type CourseGoalProp = {
+//     title : string;
+//     description : string
+// }
+// you can use Interface
+// interface CourseGoalProp{
+//     title : string;
+//     description : string
+// }
+// export default function CourseGoal({title,description} : CourseGoalProp){
+//     return <article>
+//         <div>
+//             <h2>
+//                 {title}
+//             </h2>
+//             <p>{description}</p>
+//         </div>
+//         <button>DELETE</button>
+//     </article>
+// }
+
+
+// using react default children
+interface CourseGoalProp{
+    title : string;
+    children : ReactNode
+}
+export default function CourseGoal({title,children} : CourseGoalProp){
+    return <article>
+        <div>
+            <h2>
+                {title}
+            </h2>
+            <p>{children}</p>
+        </div>
+        <button>DELETE</button>
+    </article>
+}
